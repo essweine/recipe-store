b69104c6622298c26a8e792ede59299f105548f4
Elizabeth Esswein <essweine@georgetown.edu>
2017-05-07 10:38:29 -0400

    added extraction of field list to profile builder

--- collection/profile_builder.py
+++ collection/profile_builder.py
@@ -1,4 +1,5 @@
 import requests, time
+import json, re
 from urlparse import urlparse, urljoin
 from lxml import html
 
@@ -21,10 +22,13 @@ class ProfileBuilder(object):
 
         if data.xpath("//script[@type='application/ld+json']"):
             self.extract_method = "json-ld"
+            self.fields = self.get_json_fields(data.xpath("//script[@type='application/ld+json']"))
         elif data.xpath("//*[@itemtype='http://schema.org/Recipe']"):
             self.extract_method = "microdata"
+            self.fields = self.get_html_fields(data.xpath("//*[@itemtype='http://schema.org/Recipe']"), "itemprop")
         elif data.xpath("//*[@typeof='Recipe']"):
             self.extract_method = "RDFa"
+            self.fields = self.get_html_fields(data.xpath("//*[@typeof='Recipe']"), "property")
         else:
             self.extract_method = None
 
@@ -49,10 +53,30 @@ class ProfileBuilder(object):
             "extract_method": self.extract_method,
         }
 
+    def get_json_fields(self, scripts):
+
+        for scr in scripts:
+            try:
+                recipe = json.loads(scr.text)
+            except:
+                continue
+            if not isinstance(recipe, dict):
+                continue
+            if recipe["@type"] != "Recipe":
+                continue
+            return [ key for key in recipe.keys() if not re.match("@", key) ]
+
+    def get_html_fields(self, data, attr):
+
+        return set([ item.attrib[attr] for item in data[0].xpath(".//*[@%s]" % attr) ])
+
     def __str__(self):
 
         s = "Settings detected in %s\n" % self.url
         for setting in [ "base_url", "link_prefix", "extract_method" ]:
             s += "%-16s: %s\n" % (setting, self.__dict__[setting])
+        s += "\nAvailable fields:\n"
+        s += "\n".join([ "\t%s" % field for field in sorted(self.fields) ])
+        s += "\n"
         return s
 
